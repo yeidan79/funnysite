0b7bc2ddac0ba859b0fd3e4fc41c7371b742442e
yeidan79 <1181241@isep.ipp.pt>
2020-08-17 21:08:24 +0100

    Updates

---
+++ precache-manifest.24851a466663256408821a2634d99045.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0093fa12509b73eeaff420e045f91b8",
+    "url": "/funnysite/index.html"
+  },
+  {
+    "revision": "60c038b339a13a00c1b4",
+    "url": "/funnysite/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "82369db02f197bd5a3d3",
+    "url": "/funnysite/static/css/main.e95378e7.chunk.css"
+  },
+  {
+    "revision": "60c038b339a13a00c1b4",
+    "url": "/funnysite/static/js/2.41a9d47f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/funnysite/static/js/2.41a9d47f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82369db02f197bd5a3d3",
+    "url": "/funnysite/static/js/main.d5ca00c6.chunk.js"
+  },
+  {
+    "revision": "4938d0abbc1f54a5a9f8",
+    "url": "/funnysite/static/js/runtime-main.4eb5d52b.js"
+  },
+  {
+    "revision": "69c96fca85944009c9dd72044b6252e2",
+    "url": "/funnysite/static/media/Fondamento-Regular.69c96fca.ttf"
+  }
+]);
